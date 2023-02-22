b73509b9335f2685d21c3ba3da649ed32772e1ed
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2023-02-22 18:59:35 +0700

    Update main1.js

--- main1.js
+++ main1.js
@@ -3,6 +3,7 @@ const btn = document.getElementById('form-button')
 const popup = document.getElementById('popup')
 const close_popup = document.getElementById('close-popup')
 const close_popup2 = document.getElementById('close-popup2')
+const inputField = document.getElementById('input-field')
 
 
 form.addEventListener('submit', onSubmit)
@@ -12,6 +13,10 @@ close_popup2.addEventListener('click', () => { popup.style.display = "none" })
 
 async function onSubmit (event) {
   event.preventDefault() // чтобы страница не перезагружалась при клике
+  
+  if (!inputField.value.length) return
+  
+  inputюмфдг
 
   try {
     const result = await fetch('https://jsonplaceholder.typicode.com/posts',  {
